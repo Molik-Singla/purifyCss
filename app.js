d3f7f03e1ff94e53427d33b78fe84506be0d16bd
Molik-Singla <77623428+Molik-Singla@users.noreply.github.com>
2022-01-28 23:17:50 +0530

    Pending... UI and Optimisation
    Optimisation , 
    Better responding fro user is left,
    More testing is Pending ,

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cssTestArea = document.querySelector("#css-text");
 const jsTestArea = document.querySelector("#js-text");
 const cssPurifiedTextArea = document.querySelector("#html-purifiedText");
 const cssUnusedTextArea = document.querySelector("#html-unusedText");
+const cssLinesRemoveTextArea = document.querySelector("#html-linesRemove");
 
 const submitButton = document.querySelector(".submit-btn");
 const clearAllButton = document.querySelector(".clearAll-btn");
@@ -13,7 +14,9 @@ clearAllButton.addEventListener("click", () => {
     jsTestArea.value = "";
     cssPurifiedTextArea.value = "";
     cssUnusedTextArea.value = "";
+    cssLinesRemoveTextArea.value = "";
 });
+
 let html;
 let css;
 let js;
@@ -28,10 +31,11 @@ let allHtmlClasses = [];
 
 let cssLinesCount = 0;
 
-let collectedUnusedCss = {};
-let countForCollectedUnusedCss = 0;
+let collectedUnusedCss = [];
+let unusedPropertyLineNumbers = [];
+// let countForCollectedUnusedCss = 0;
 
-let cssLineByLineObject = {};
+let cssPropertyByProperty = {};
 
 function getIds(characterIndex) {
     let start = characterIndex;
@@ -71,6 +75,7 @@ submitButton.addEventListener("click", () => {
     html = htmlTestArea.value;
     css = cssTestArea.value;
     js = jsTestArea.value;
+
     allCssIds = [];
     allCssClasses = [];
 
@@ -81,12 +86,12 @@ submitButton.addEventListener("click", () => {
 
     cssLinesCount = 0;
 
-    collectedUnusedCss = {};
-    countForCollectedUnusedCss = 0;
+    collectedUnusedCss = [];
 
-    cssLineByLineObject = {};
+    cssPropertyByProperty = {};
+    unusedPropertyLineNumbers = [];
 
-    // Calling getIds and getClasses function
+    // Calling getIds and getClasses function for extracting ids and classes from css
     for (let i = 0; i < css.length; i++) {
         if (css[i] === "#") {
             if (css.indexOf("{", i + 1) === -1) {
@@ -101,10 +106,11 @@ submitButton.addEventListener("click", () => {
         }
     }
 
+    // remove duplicate ids and classes
     allCssIds = [...new Set(allCssIds)];
     allCssClasses = [...new Set(allCssClasses)];
 
-    // for unused allCssIds
+    // getting unused ids from ccss ids array
     let allCssIdsCopy = [];
     allCssIds.forEach(singleId => {
         allCssIdsCopy.push(singleId.slice(1));
@@ -125,7 +131,7 @@ submitButton.addEventListener("click", () => {
         else unUsedCssIds.push(allCssIds[i]);
     }
 
-    // for all unused allCssClasses
+    // for all unused classes from css classes array
     const regex = /(?:class|className)=(?:["']\W+\s*(?:\w+)\()?["']([^'"]+)['"]/gim;
     let m;
 
@@ -159,18 +165,12 @@ submitButton.addEventListener("click", () => {
         }
     }
 
-    // count cssLinesCount in css file
-    for (let i = 0; i < css.length; i++) {
-        if (css[i] === "\n") {
-            ++cssLinesCount;
-        }
-    }
-
-    cssLineByLineObject = {};
+    cssPropertyByProperty = {};
     let starting = 0;
     let previous = 0;
     let purifiedCss = "";
 
+    // basically combined unused ids and classes in one single variable ( usefull for iterating )
     let combinesUseless = [...unUsedCssClasses, ...unUsedCssIds];
 
     // after having unused properties from html we test them if they are in javascript ( else totally unused , delete them , if in js then delete it from unused array)
@@ -190,70 +190,106 @@ submitButton.addEventListener("click", () => {
         }
     }
 
+    // extract code by code or whole property by property of css in out object
     for (let i = 0; i < css.length; i++) {
         if (css[i] === "}") {
             starting++;
             let forCorrectionMedia = css.slice(previous, i + 1);
-            cssLineByLineObject[starting] = forCorrectionMedia;
+            cssPropertyByProperty[starting] = forCorrectionMedia;
             previous = i + 1;
         }
     }
 
-    for (let i in cssLineByLineObject) {
-        purifiedCss += cssLineByLineObject[i];
-    }
-    for (let useLessStuff of combinesUseless) {
-        for (let i in cssLineByLineObject) {
-            if (cssLineByLineObject[i].includes(useLessStuff)) {
-                let splitter = cssLineByLineObject[i].split(",");
-                if (splitter.length > 1) {
-                    let answer = "";
+    purifiedCss = "";
 
-                    for (let inside = 0; inside < splitter.length; inside++) {
-                        if (!splitter[inside].includes(useLessStuff) && splitter[inside].indexOf("{") >= 0) {
-                            answer += splitter[inside];
+    // Main Logic : extraction every unused property ,
+    // collecting them in an array ,
+    // Update our cssPropertyByProperty variale as it must only contains used properties
+    for (let unusedStuff of combinesUseless) {
+        for (let i in cssPropertyByProperty) {
+            cssPropertyByProperty[i] = cssPropertyByProperty[i].trim();
+            // if unused property are included in our css then in :
+            if (cssPropertyByProperty[i].includes(unusedStuff)) {
+                let beforeBrace = cssPropertyByProperty[i].slice(0, cssPropertyByProperty[i].indexOf("{"));
+                // here we check every code property that if it only contain unused id , class or it also contain used id , class
+                if (beforeBrace.includes(",")) {
+                    let splitter = beforeBrace.split(",");
+                    for (let j = 0; j < splitter.length; j++) {
+                        if (unusedStuff == splitter[j].trim() && cssPropertyByProperty[i].includes(splitter[j].trim())) {
+                            cssPropertyByProperty[i] = cssPropertyByProperty[i].replace(unusedStuff, "");
+
+                            let beforeBraceInside = cssPropertyByProperty[i].slice(0, cssPropertyByProperty[i].indexOf("{"));
+                            let copy = beforeBraceInside.trim();
+                            if (copy[copy.length - 1] === ",") {
+                                if (copy[0] === ".") copy = copy.slice(0, copy.length - 1);
+                                else copy = copy.slice(1, copy.length - 1);
+                            } else {
+                                if (copy[0] === ".") copy = copy.slice(0, copy.length);
+                                else copy = copy.slice(1, copy.length);
+                            }
+                            cssPropertyByProperty[i] = cssPropertyByProperty[i].replace(beforeBraceInside, copy);
+                            //  collect unused css
+                            let codeBlockOfUnused = cssPropertyByProperty[i].slice(
+                                cssPropertyByProperty[i].indexOf("{"),
+                                cssPropertyByProperty[i].indexOf("}") + 1
+                            );
+                            let flag = 0;
+                            for (let i = 0; i < collectedUnusedCss.length; i++) {
+                                if (collectedUnusedCss[i].includes(codeBlockOfUnused)) {
+                                    let ind = collectedUnusedCss[i].indexOf("{");
+                                    let ans = collectedUnusedCss[i].slice(0, ind) + " , " + unusedStuff + collectedUnusedCss[i].slice(ind);
+                                    collectedUnusedCss[i] = ans;
+                                    flag = 1;
+                                }
+                            }
+                            if (flag === 0) {
+                                collectedUnusedCss.push(unusedStuff + codeBlockOfUnused + "\n");
+                            }
                         }
-                    }
-                    if (answer.length > 0) {
-                        if (answer.includes("{") && answer.includes(useLessStuff)) {
-                            purifiedCss = purifiedCss.replace(cssLineByLineObject[i], answer);
-                            console.log(answer);
-                            console.log(cssLineByLineObject[i]);
-                            collectedUnusedCss[++countForCollectedUnusedCss] = cssLineByLineObject[i];
-                        } else {
-                            answer = answer + cssLineByLineObject[i].slice(cssLineByLineObject[i].indexOf("{"), cssLineByLineObject[i].indexOf("}") + 1);
-                            purifiedCss = purifiedCss.replace(cssLineByLineObject[i], answer);
-                            console.log(cssLineByLineObject[i]);
-                            collectedUnusedCss[++countForCollectedUnusedCss] = cssLineByLineObject[i];
-                        }
-                    } else {
-                        purifiedCss = purifiedCss.replace(cssLineByLineObject[i], answer);
-                        console.log(cssLineByLineObject[i]);
-                        collectedUnusedCss[++countForCollectedUnusedCss] = cssLineByLineObject[i];
                     }
                 } else {
-                    if (cssLineByLineObject[i].includes("@media")) {
-                        let fakeVar = cssLineByLineObject[i].slice(cssLineByLineObject[i].indexOf("{") + 1);
-                        purifiedCss = purifiedCss.replace(fakeVar, "");
-                        collectedUnusedCss[++countForCollectedUnusedCss] = fakeVar;
+                    // else if only one class , ids was there then delete or remove it as it is unused
+                    // settings for if our code contains @ ( media or keyframes )
+                    if (cssPropertyByProperty[i].includes("@")) {
+                        let adding = cssPropertyByProperty[i].slice(0, cssPropertyByProperty[i].indexOf("{") + 1);
+                        cssPropertyByProperty[i] = cssPropertyByProperty[i].slice(cssPropertyByProperty[i].indexOf("{") + 1);
+                        collectedUnusedCss.push(cssPropertyByProperty[i].trim());
+                        cssPropertyByProperty[i] = adding;
                     } else {
-                        purifiedCss = purifiedCss.replace(cssLineByLineObject[i], "");
-                        collectedUnusedCss[++countForCollectedUnusedCss] = cssLineByLineObject[i];
+                        collectedUnusedCss.push(cssPropertyByProperty[i].trim());
+                        cssPropertyByProperty[i] = "";
                     }
                 }
             }
         }
     }
 
-    // returned this purifies css , unused css
-    // console.log("---------------  Purified Css  -----------------");
-    // console.log(purifiedCss);
-    // console.log("---------------  Unused Css  -----------------");
-    let cssWhichWasUnused = "";
-    for (let singleUnused in collectedUnusedCss) {
-        // console.log(collectedUnusedCss[singleUnused].trim());
-        cssWhichWasUnused += collectedUnusedCss[singleUnused].trim() + "\n";
+    // purified css
+    for (let i in cssPropertyByProperty) {
+        purifiedCss += cssPropertyByProperty[i].trim() + "\n";
+    }
+
+    // unused css
+    for (let item of collectedUnusedCss) {
+        cssUnusedTextArea.value += item + "\n";
+    }
+
+    let strt = 0;
+    for (let i = 0; i < css.length; i++) {
+        if (css[i] === "\n") {
+            ++cssLinesCount;
+            let answer = css.slice(strt, i);
+            strt = i;
+            for (let item of combinesUseless) {
+                if (answer.includes(item)) {
+                    unusedPropertyLineNumbers.push(cssLinesCount);
+                }
+            }
+        }
+    }
+
+    for (let item of unusedPropertyLineNumbers) {
+        cssLinesRemoveTextArea.value += item + " , ";
     }
     cssPurifiedTextArea.value = purifiedCss;
-    cssUnusedTextArea.value = cssWhichWasUnused;
 });
